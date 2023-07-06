223c4bafc585a060cf5ef290672df0f59397adf7
Suhas Masal <suhasmasal100@gmail.com>
2023-07-06 11:26:34 +0530

    my first commit

--- src/main/java/net/javanew/springbootbackend/repository/StudentRepository.java
+++
@@ -1,9 +0,0 @@
-package net.javanew.springbootbackend.repository;
-
-import net.javanew.springbootbackend.model.Student;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-
-public interface StudentRepository extends JpaRepository<Student, Long> {
-}
